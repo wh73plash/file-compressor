631b860360b41e403c02fa271d684a627c5b5a3e
JeinKim <wh73plash@gmail.com>
2022-06-13 07:36:59 +0900

    initialize

---
+++ file-compressor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  file-compressor
+//
+//  Created by 김제인 on 2022/06/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
